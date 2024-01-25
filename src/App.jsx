09a4236377732896d2b754e100ff977b8917621a
kstallworth2012 <kennethstallworth@hotmail.com>
2024-01-24 22:52:39 -0500

    My Little React Template

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes }from './routes'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Vite + React Front End Template</h1>
+
+        <Routes />
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
